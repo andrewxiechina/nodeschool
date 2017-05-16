991a9b3ac0e870e31948a6e97d124690e605ec95
Andrew Y Xie <andyxie@D-69-91-233-201.dhcp4.washington.edu>
2017-05-16 15:29:00 -0700

    not working async

--- node_http.js
+++ node_http.js
@@ -1,18 +1,29 @@
-const http = require('http');
 
-http.get(process.argv[2], (res) => {
-  res.setEncoding('utf8');
-  let rawData = '';
-  res.on('data', (chunk) => { rawData += chunk;});
-  res.on('end', () => {
-    try {
-      const parsedData = rawData;
-      console.log(parsedData.length);
-      console.log(parsedData);
-    } catch (e) {
-      console.error(e.message);
-    }
+
+function getData(url, callback){
+
+  http.get(url, (res) => {
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => { rawData += chunk;});
+    res.on('end', () => {
+      try {
+        const parsedData = rawData;
+        callback(parsedData);
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  }).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
   });
-}).on('error', (e) => {
-  console.error(`Got error: ${e.message}`);
+}
+
+var count = 0;
+obj = {}
+getData(process.argv[2], (data) => {
+
+  if count == 3; {
+
+  }
 });
