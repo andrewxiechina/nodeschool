6f5f93c3782fe5f948b7bd57664ec425fed32fe3
andy <andrewcrab.china@gmail.com>
2017-05-16 19:32:12 -0700

    stylus

--- express/hello.js
+++ express/hello.js
@@ -2,10 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.post('/form', function(req, res) {
-   res.send(req.body.str.split("").reverse().join(""));
-});
-
+app.use('/', express.static(process.argv[3]));
+app.use('/', require('stylus').middleware(process.argv[3]));
 app.listen(process.argv[2] || 3000);
